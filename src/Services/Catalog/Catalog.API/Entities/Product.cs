84990b5294dabf03f015aec15bbd7eb86c47b2db
Mostafa Kamel <Mostafa.kamel@sidpec.com>
2024-10-30 14:35:24 +0300

    Create Product command

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }=string.Empty;
+        public string Image { get; set; }=string.Empty;
+        public double Price { get; set; }
+        public string Description { get; set; }=string.Empty;
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,List<string> Categories
+        ,string Description,string Image,double Price):IRequest<CreateProductResult>;
+  
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
