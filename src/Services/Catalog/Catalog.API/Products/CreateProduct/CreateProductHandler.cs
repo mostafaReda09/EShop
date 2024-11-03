9798355e7ee7deaf5dcc416330836b415518f36d
Mostafa Kamel <Mostafa.kamel@sidpec.com>
2024-11-03 14:29:57 +0200

    add docker compose and postgres

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -3,6 +3,7 @@ using Carter;
 using Catalog.API.Entities;
 using EShop.Shared.CQRS;
 using Mapster;
+using Marten;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
@@ -29,13 +30,14 @@ namespace Catalog.API.Products.CreateProduct
                
         }
     }
-    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             var product=command.Adapt<Product>();   
-           
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new CreateProductResult(product.Id);
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,16 @@
 using Carter;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
 builder.Services.AddMediatR(cfg=>
     cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly)
 );
+builder.Services.AddMarten(opt => 
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Default")!);
+}).UseLightweightSessions();
 var app = builder.Build();
 app.MapCarter();
 app.Run();
+
