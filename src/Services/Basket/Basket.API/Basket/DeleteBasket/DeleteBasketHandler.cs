f395d3eb20745c0d00353141c5ba8a04432f398c
Mostafa Kamel <Mostafa.kamel@sidpec.com>
2024-11-19 10:35:28 +0200

    register service In DI container

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -11,7 +11,7 @@ namespace Basket.API.Basket.DeleteBasket
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapDelete("/basket", async (string userName, ISender sender) =>
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
             {
 
                 var result = await sender.Send(new DeleteBasketCommand(userName));
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,22 @@
 using Carter;
+using EShop.Shared.Behaviors;
+using EShop.Shared.Exceptions.Handlers;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
-var app = builder.Build();
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
 
+});
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+var app = builder.Build();
+app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
