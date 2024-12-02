a0c180dbc6175d47802d677d165b12e6292a4a81
Mostafa Kamel <Mostafa.kamel@sidpec.com>
2024-12-02 10:44:42 +0200

    createing ordering service

---
+++ src/Services/Ordering/Ordering.API/APIServicesRegisteration.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.API
+{
+    public static class APIServicesRegisteration
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
